8f9c93a440c2e3bf3839976876f471f513bb8f0b
Piyush Pushkar <149616014+piyush0189@users.noreply.github.com>
2025-09-06 10:55:55 +0000

    Sync LeetCode submission Runtime - 1 ms (99.72%), Memory - 44 MB (99.98%)

---
+++ 0443-string-compression/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0; 
+        int i = 0;     
+
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            
+            while (i < chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+
+            
+            chars[index++] = currentChar;
+
+        
+            if (count > 1) {
+                String countStr = String.valueOf(count);
+                for (char c : countStr.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index; 
+    }
+}
+
