f0febb8b057cc6cb1854ca4a3afd81229bd79130
Piyush Pushkar <149616014+piyush0189@users.noreply.github.com>
2026-01-27 14:24:30 +0000

    Sync LeetCode submission Runtime - 1 ms (60.04%), Memory - 48.7 MB (23.16%)

--- 0042-trapping-rain-water/solution.java
+++ 0042-trapping-rain-water/solution.java
@@ -5,17 +5,17 @@ class Solution {
         leftMax[0] = height[0];
         for(int i=1; i<n; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i-1]);
-        } 
+        }
         int rightMax[] = new int[n];
-        rightMax[n-1] = height[n-1];
+         rightMax[n-1] = height[n-1];
         for(int i=n-2; i>=0; i--) {
             rightMax[i] = Math.max(height[i], rightMax[i+1]);
         }
-        int tappedWater = 0;
+        int trappedWater = 0;
         for(int i=0; i<n; i++) {
             int waterLevel = Math.min(leftMax[i], rightMax[i]);
-            tappedWater += waterLevel - height[i];
+            trappedWater += waterLevel - height[i];
         }
-        return tappedWater;
+        return trappedWater;
     }
 }
