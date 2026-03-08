8f08e8fcb8bdb922b53ccf0efbfdea621ed45a5b
Piyush Pushkar <149616014+piyush0189@users.noreply.github.com>
2026-03-08 11:50:48 +0000

    Sync LeetCode submission Runtime - 3 ms (99.74%), Memory - 94.1 MB (95.16%)

---
+++ 0234-palindrome-linked-list/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) {
+            return true;
+        }
+        // find mid
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // Reverse 2nd half
+        ListNode prev = null;
+        ListNode curr = slow;
+        ListNode next;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        // compare halves
+        ListNode left = head;
+        ListNode right = prev;
+
+        while(right != null) {
+            if(left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+}
